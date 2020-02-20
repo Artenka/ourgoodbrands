03385c0604b5ef4b8e9db34a7921b80f9a7e1c65
Artem <artemgergetc@gmail.com>
2020-02-20 23:37:57 +0200

    project init; landing page finished

---
+++ gulpfile.babel.js
@@ -0,0 +1,38 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import jade from 'gulp-jade';
+import rename from 'gulp-rename';
+import autoprefixer from 'gulp-autoprefixer';
+var webserver = require('gulp-webserver');
+
+gulp.task('server', function() {
+	gulp.src('./')
+			.pipe(webserver({
+				livereload: true,
+				directoryListing: true,
+				open: true
+			}));
+});
+
+gulp.task('sass', () => {
+	return gulp.src('./source/markup/sass/**/*.scss')
+		.pipe(sass())
+		.pipe(autoprefixer({ browsers:['last 4 versions'] }))
+		.pipe(gulp.dest('./public/css'))
+});
+
+gulp.task('jade', () => {
+	gulp.src('./source/markup/jade/views/*.jade')
+		.pipe(jade({pretty: true}))
+		.pipe(rename({
+			extname: '.php'
+		}))
+		.pipe(gulp.dest('./public'))
+});
+
+gulp.task('build', ['sass', 'jade']);
+
+gulp.task('default', ['sass', 'jade'], () => {
+	gulp.watch('./source/markup/jade/**/*.jade', ['jade']);
+	gulp.watch('./source/markup/sass/**/*.scss', ['sass']);
+});
